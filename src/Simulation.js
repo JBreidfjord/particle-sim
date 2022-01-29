a9dce2d43a80725a829e3d74d10b5edc05dc5103
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-01-28 19:30:24 -0700

    Handle resizing with pixel ratio for clear images

--- src/Simulation.js
+++ src/Simulation.js
@@ -2,6 +2,18 @@ import "./Simulation.css";
 
 import { useEffect, useRef } from "react";
 
+const getPixelRatio = (ctx) => {
+  let backingStore =
+    ctx.backingStorePixelRatio ||
+    ctx.webkitBackingStorePixelRatio ||
+    ctx.mozBackingStorePixelRatio ||
+    ctx.msBackingStorePixelRatio ||
+    ctx.oBackingStorePixelRatio ||
+    1;
+
+  return (window.devicePixelRatio || 1) / backingStore;
+};
+
 export default function Simulation() {
   const canvasRef = useRef(null);
 
@@ -9,13 +21,33 @@ export default function Simulation() {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
 
-    const width = canvas.width;
-    const height = canvas.height;
+    const setScale = () => {
+      const ratio = getPixelRatio(ctx);
+      const width = Math.round(window.innerWidth * 0.75);
+      const height = Math.round(window.innerHeight * 0.75);
 
-    ctx.fillStyle = "#000";
-    ctx.beginPath();
-    ctx.arc(width / 2, height / 2, 10, 0, Math.PI * 2);
-    ctx.fill();
+      const needResize = canvas.width !== width * ratio || canvas.height !== height * ratio;
+
+      if (needResize) {
+        canvas.width = width * ratio;
+        canvas.height = height * ratio;
+        canvas.style.width = `${width}px`;
+        canvas.style.height = `${height}px`;
+      }
+    };
+
+    const render = () => {
+      setScale();
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      ctx.beginPath();
+      ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 4, 0, 2 * Math.PI);
+      ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+      ctx.fill();
+    };
+
+    let requestId = requestAnimationFrame(render);
+    return () => cancelAnimationFrame(requestId);
   }, []);
 
   return (
