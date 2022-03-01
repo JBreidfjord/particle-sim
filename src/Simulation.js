28138c1c706393c501d8f9d77b83f4915ef0a4f4
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-03-01 16:09:25 -0700

    Fix errors from merge

--- src/Simulation.js
+++ src/Simulation.js
@@ -87,7 +87,6 @@ export default function Simulation() {
 
   // Handle running simulation
   const fps = 120;
-  const speedMult = 1;
   useEffect(() => {
     const step = () => {
       setParticles((prevParticles) => {
@@ -165,33 +164,35 @@ export default function Simulation() {
         <canvas ref={canvasRef}></canvas>
         <div className="stats">
           {particles && elapsed > 0 && (
-            <p>
-              {/* P = F / A, F = Δp / Δt */}
-              {/* A = 1 when container wall length = 1 */}
-              Simulated Pressure:{" "}
-              {(
-                particles.reduce(
-                  (momentumTransferred, particle) =>
-                    momentumTransferred + particle.momentumTransferred,
-                  0
-                ) / elapsed
-              ).toFixed(5)}
-            </p>
-            <p>Elapsed: {elapsed.toFixed(2)}s</p>
-            <p>
-              {/* P = (2N / 3V)(KEavg), where N is the number of particles */}
-              {/* V = 1 when container wall length = 1 */}
-              Calculated Pressure:{" "}
-              {(
-                ((2 * numParticles) / 3) *
-                (particles.reduce(
-                  (kineticEnergy, particle) => kineticEnergy + Particle.kineticEnergy(particle),
-                  0
-                ) /
-                  numParticles)
-              ).toFixed(5)}
-            </p>
-          )}  
+            <>
+              <p>
+                {/* P = F / A, F = Δp / Δt */}
+                {/* A = 1 when container wall length = 1 */}
+                Simulated Pressure:{" "}
+                {(
+                  particles.reduce(
+                    (momentumTransferred, particle) =>
+                      momentumTransferred + particle.momentumTransferred,
+                    0
+                  ) / elapsed
+                ).toFixed(5)}
+              </p>
+              <p>Elapsed: {elapsed.toFixed(2)}s</p>
+              <p>
+                {/* P = (2N / 3V)(KEavg), where N is the number of particles */}
+                {/* V = 1 when container wall length = 1 */}
+                Calculated Pressure:{" "}
+                {(
+                  ((2 * numParticles) / 3) *
+                  (particles.reduce(
+                    (kineticEnergy, particle) => kineticEnergy + Particle.kineticEnergy(particle),
+                    0
+                  ) /
+                    numParticles)
+                ).toFixed(5)}
+              </p>
+            </>
+          )}
         </div>
       </div>
     </div>
