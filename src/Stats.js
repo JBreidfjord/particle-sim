a100a424d6debbdc6660d0eaf245daefe0a8e86c
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-03-28 08:40:14 -0600

    Add units to pressure

--- src/Stats.js
+++ src/Stats.js
@@ -8,14 +8,20 @@ export default function Stats({ particles, elapsed }) {
           <p>
             {/* P = F / A, F = Δp / Δt */}
             {/* A = 1 when container wall length = 1 */}
-            Simulated Pressure: {(Particle.momentumTransferred(particles) / elapsed / 4).toFixed(5)}
+            Simulated Pressure:{" "}
+            {(Particle.momentumTransferred(particles) / elapsed / 4 / 1000).toExponential(3)} kPa
           </p>
           <p>Elapsed: {elapsed.toFixed(2)}s</p>
           <p>
             {/* P = K/A, where K is the total kinetic energy of the particles */}
             {/* A = 1 when container wall length = 1 */}
             Calculated Pressure:{" "}
-            {(Particle.totalKineticEnergy(particles) / (1 - Particle.area(particles))).toFixed(5)}
+            {(
+              Particle.totalKineticEnergy(particles) /
+              (1 - Particle.area(particles)) /
+              1000
+            ).toExponential(3)}{" "}
+            kPa
           </p>
         </>
       )}
