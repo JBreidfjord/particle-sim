ba2051c5ea02b7eff0ec2bae75af2a516d4940f5
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-03-29 14:48:52 -0600

    Optimize grid size calculation

--- libs/simulation-core/src/particle.rs
+++ libs/simulation-core/src/particle.rs
@@ -56,7 +56,9 @@ impl Particle {
         // Uniform Grid Partition
         // Create a grid of particles
         let mut grid = Vec::new();
-        let grid_size = (particles.len() as f64).sqrt().ceil() as usize;
+        let grid_size = ((particles.len() as f64).log(1.6572725) - 3.89293267)
+            .ceil()
+            .max(2.0) as usize;
         let grid_width = 1.0 / grid_size as f64;
 
         // Check for particles within each grid cell
@@ -339,5 +341,18 @@ mod tests {
         assert_eq!(pairs.len(), expected_pairs.len());
         let matching = get_matching_count(&pairs, &expected_pairs);
         assert!(matching == pairs.len() && matching == expected_pairs.len());
+    }
+
+    #[test]
+    fn test_generate_pairs_performance() {
+        use std::time::SystemTime;
+        let num_particles = 100_000;
+        let particles = Particle::generate_particles(num_particles, 0.0002);
+        let start = SystemTime::now();
+        let _pairs = Particle::generate_pairs(particles);
+        let end = SystemTime::now();
+        let duration = end.duration_since(start).unwrap();
+        println!("Time taken for {} particles: {:?}", num_particles, duration);
+        panic!("");
     }
 }
